9ea342897ad7473e016bca884d0403260da362f3
joselomartinez87 <martinezjose@javeriana.edu.co>
2018-08-09 15:38:49 -0500

    Eliminacion archivos migrate español

--- Backend/DenunciaEficiente/db/migrate/20180731014900_unidades_policias.rb
+++
@@ -1,8 +0,0 @@
-class UnidadesPolicias < ActiveRecord::Migration[5.2]
-  def change
-    create_table :unidades_policias do |t|
-      t.string :nombre
-      t.timestamps null: false 
-    end
-  end
-end
--- Backend/DenunciaEficiente/db/migrate/20180731014944_testigos.rb
+++
@@ -1,9 +0,0 @@
-class Testigos < ActiveRecord::Migration[5.2]
-  def change
-    create_table :testigos do |t|
-      t.belongs_to :usuario, index: true, foreign_key: true
-      t.bigint :denuncia_id, null: false
-      t.timestamps null: false
-    end
-  end
-end
